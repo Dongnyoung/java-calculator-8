008f0a1181ab4fdd73201cd3661bbfdb6ba6daf3
Dongnyoung <leedo7182@naver.com>
2025-10-14 22:23:54 +0900

    feat: 계산기 실행 기본 구조 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,27 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
+class Calculator {
+    public void run() {
+        openingMent();
+        //입력받기
+        String input = Console.readLine();
+
+        //결과출력
+        int output =0;
+        System.out.println("결과 : "+output);
+    }
+    public void openingMent(){
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+    }
+}
+
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Calculator calc = new Calculator();
+        calc.run();
     }
 }
