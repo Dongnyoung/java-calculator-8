65178a5818abd09428bbef5f8ceaa1f9939a1d8a
Dongnyoung <leedo7182@naver.com>
2025-10-15 15:17:57 +0900

    fix: 직관성있게, char배열로 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -26,18 +26,38 @@ class Calculator {
         String commaColonRule = "[,:]"; //정규식에서는 [,]는 포함되지않는다
         //커스텀구분자 체크
         String firstCustomRule = "//";
-        String secondCustomRule = "\\n";
+
         if(str.startsWith(firstCustomRule)) {
             int firstCustomRuleIndex = str.indexOf(firstCustomRule);
-            if(!str.contains(secondCustomRule)){
+            char[] charList = str.toCharArray();
+            boolean flag = false;
+            int secondCustomRuleIndex=0;
+            int endIndex = 0;
+            for(int i=2;i<charList.length;i++) {
+                if(charList[i]=='\\') {
+                    if (i+1<charList.length && charList[i + 1] == 'n') {
+                        flag = true;
+                        endIndex = i+2;
+                        secondCustomRuleIndex=i-1;
+                        break;
+                    }
+                }
+
+
+            }
+            if(!flag){
                 throw new IllegalArgumentException("잘못된 형식의 커스텀 구분자");
             }
-            int secondCustomRuleIndex = str.indexOf(secondCustomRule);
-            String custom = str.substring(firstCustomRuleIndex+2,secondCustomRuleIndex);//커스텀 구분자
+
+            String custom = String.valueOf(charList[secondCustomRuleIndex]);//커스텀 구분자
             if(custom.length()!=1){
                 throw new IllegalArgumentException("커스텀 구분자는 단일 문자만 허용");
             }
-            str = inputString.substring(secondCustomRuleIndex+2);
+            if(endIndex == charList.length){
+                //빈 문자열
+                return 0;
+            }
+            str = inputString.substring(endIndex);
             String[] tokens = str.split(custom);
             for (String t : tokens) {
                 if (t.isEmpty()) {
