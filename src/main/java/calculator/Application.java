f782adafdbbd179a4a8f5e742fe4f4fbcbf7b55d
Dongnyoung <leedo7182@naver.com>
2025-10-17 03:57:58 +0900

    refactor: 파서 선택 로직을 resolver 메소드로 분리
    기존 if/else문을 제거하고, 전략패턴을 이용하여 리팩토링하였음.
    입력문자열에 따라 파서를 반환하는 resolver 메서드로 분리함.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,6 +3,7 @@ package calculator;
 import calculator.domain.Validator;
 import calculator.domain.parser.CommaColonParser;
 import calculator.domain.parser.CustomParser;
+import calculator.domain.parser.Parser;
 import calculator.io.Input;
 import calculator.io.Output;
 import camp.nextstep.edu.missionutils.Console;
@@ -15,8 +16,8 @@ class Calculator {
     private Input input = new Input();
     private Output output = new Output();
     private Validator validator = new Validator();
-    private CustomParser customParser = new CustomParser();
-    private CommaColonParser commaColonParser = new CommaColonParser();
+    private Parser parser;
+
     public void run() {
         openingMent();
         //입력받기
@@ -34,19 +35,13 @@ class Calculator {
             return 0;
         }
         int sum = 0;
-        //입력검사
-        //콤마,콜론
-        String commaColonRule = "[,:]"; //정규식에서는 [,]는 포함되지않는다
-        //커스텀구분자 체크
-        String firstCustomRule = "//";
-
-        if(str.startsWith(firstCustomRule)) {
-            String[] tokens = customParser.parsing(str);
-            for (String t : tokens) {
-                if (t.isEmpty()) {
-                    throw new IllegalArgumentException("구분자 사이에 빈 문자열이 있음");
-                }
-                if (!t.matches("\\d+")) // "\\d+"는 정규식, 숫자 한개이상으로만 포함된 문자열
+        Parser parser = resolver(str);
+        String[] tokens = parser.parsing(str);
+        for (String t : tokens) {
+            if (t.isEmpty()) {
+                throw new IllegalArgumentException("구분자 사이에 빈 문자열이 있음");
+            }
+            if (!t.matches("\\d+")) // "\\d+"는 정규식, 숫자 한개이상으로만 포함된 문자열
                 {
                     if (t.matches("-\\d+")) {
                         throw new IllegalArgumentException("음수는 허용되지않음");
@@ -55,25 +50,17 @@ class Calculator {
                 }
                 sum += Integer.parseInt(t);
             }
-        }
-        else {
-            String[] tokens = commaColonParser.parsing(str);
-
-            for (String t : tokens) {
-                if (t.isEmpty()) {
-                    throw new IllegalArgumentException("구분자 사이에 빈 문자열이 있음");
-                }
-                if (!t.matches("\\d+")) // "\\d+"는 정규식, 숫자 한개이상으로만 포함된 문자열
-                {
-                    if (t.matches("-\\d+")) {
-                        throw new IllegalArgumentException("음수는 허용되지않음");
-                    }
-                    throw new IllegalArgumentException("숫자가 아닌 값이 포함되어 있음 " + t);
-                }
-                sum += Integer.parseInt(t);
-            }
-        }
         return sum;
+    }
+    private Parser resolver(String input) {
+        // 커스텀 규칙 확인
+        String customFirstRule = "//";
+        if (input.startsWith(customFirstRule)) {
+            return new CustomParser();
+        } else {
+            return new CommaColonParser();
+        }
+
     }
 
     public void openingMent(){
