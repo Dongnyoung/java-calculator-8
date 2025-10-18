49c7739807ebb1a1a50ad89924a9fc59f58cb00b
Dongnyoung <leedo7182@naver.com>
2025-10-18 22:15:23 +0900

    feat: 구분자 사이 빈 문자열 검증 클래스 추가

---
+++ src/main/java/calculator/domain/TokenValidator/NullValidator.java
@@ -0,0 +1,13 @@
+package calculator.domain.TokenValidator;
+
+public class NullValidator implements TokenValidator {
+    @Override
+    public String[] valid(String[] tokens) {
+        for (String t : tokens) {
+            if (t.isEmpty()) {
+                throw new IllegalArgumentException("구분자 사이에 빈 문자열이 있음");
+            }
+        }
+        return tokens;
+    }
+}
