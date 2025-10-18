8067a82d626104c41eeb463e2ecf3ba97b9dce0b
Dongnyoung <leedo7182@naver.com>
2025-10-18 19:26:37 +0900

    refactor: Add클래스셍성 및 로직분리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,6 +1,6 @@
 package calculator;
 
-import calculator.domain.Validator;
+import calculator.domain.InputValidator;
 import calculator.domain.parser.CommaColonParser;
 import calculator.domain.parser.CustomParser;
 import calculator.domain.parser.Parser;
@@ -9,15 +9,33 @@ import calculator.io.Output;
 import camp.nextstep.edu.missionutils.Console;
 
 
-
+class Add{
+    private int sum;
+    public int calc(String[] tokens) {
+        for (String t : tokens) {
+            if (t.isEmpty()) {
+                throw new IllegalArgumentException("구분자 사이에 빈 문자열이 있음");
+            }
+            if (!t.matches("\\d+")) // "\\d+"는 정규식, 숫자 한개이상으로만 포함된 문자열
+            {
+                if (t.matches("-\\d+")) {
+                    throw new IllegalArgumentException("음수는 허용되지않음");
+                }
+                throw new IllegalArgumentException("숫자가 아닌 값이 포함되어 있음 " + t);
+            }
+            sum += Integer.parseInt(t);
+        }
+        return sum;
+    }
+}
 
 
 class Calculator {
     private Input input = new Input();
     private Output output = new Output();
-    private Validator validator = new Validator();
+    private InputValidator inputValidator = new InputValidator();
     private Parser parser;
-
+    private Add add = new Add();
     public void run() {
         openingMent();
         //입력받기
@@ -30,26 +48,14 @@ class Calculator {
     }
 
     private int parse(String inputString) {
-        String str = validator.valid(inputString);
+        String str = inputValidator.valid(inputString);
         if(str.equals("0")){
             return 0;
         }
         int sum = 0;
         Parser parser = resolver(str);
         String[] tokens = parser.parsing(str);
-        for (String t : tokens) {
-            if (t.isEmpty()) {
-                throw new IllegalArgumentException("구분자 사이에 빈 문자열이 있음");
-            }
-            if (!t.matches("\\d+")) // "\\d+"는 정규식, 숫자 한개이상으로만 포함된 문자열
-                {
-                    if (t.matches("-\\d+")) {
-                        throw new IllegalArgumentException("음수는 허용되지않음");
-                    }
-                    throw new IllegalArgumentException("숫자가 아닌 값이 포함되어 있음 " + t);
-                }
-                sum += Integer.parseInt(t);
-            }
+        sum = add.calc(tokens);
         return sum;
     }
     private Parser resolver(String input) {
