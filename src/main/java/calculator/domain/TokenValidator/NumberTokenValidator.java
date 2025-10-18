62e226d39d5b8018f3a444861caece1b03cc660b
Dongnyoung <leedo7182@naver.com>
2025-10-18 22:11:28 +0900

    feat: 토큰 숫자 체크

---
+++ src/main/java/calculator/domain/TokenValidator/NumberTokenValidator.java
@@ -0,0 +1,18 @@
+package calculator.domain.TokenValidator;
+
+public class NumberTokenValidator implements TokenValidator {
+    @Override
+    public String[] valid(String[] tokens) {
+        for (String t : tokens) {
+            if (!t.matches("\\d+")) // "\\d+"는 정규식, 숫자 한개이상으로만 포함된 문자열
+            {
+                if (t.matches("-\\d+")) {
+                    throw new IllegalArgumentException("음수는 허용되지않음");
+                }
+                throw new IllegalArgumentException("숫자가 아닌 값이 포함되어 있음 " + t);
+            }
+
+        }
+        return tokens;
+    }
+}
